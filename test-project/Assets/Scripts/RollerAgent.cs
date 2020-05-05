61c84f88ae44960252808ccf5984e4a50f6fc367
Marc de Pedro <marcdp2012@gmail.com>
2020-05-05 17:08:55 +0200

    Jump implemented

--- test-project/Assets/Scripts/RollerAgent.cs
+++ test-project/Assets/Scripts/RollerAgent.cs
@@ -4,16 +4,22 @@ using MLAgents;
 public class RollerAgent : Agent
 {
     public Transform target = null;
+    public Transform wall = null;
     public float speed = 10;
+    public float jumpForce = 200;
     public TextMesh debugText;
 
     private Vector3 initialPos;
+    private Vector3 targetInitialPos;
+
     private Rigidbody rBody;
+    private bool isGrounded = true;
 
     void Start()
     {
         rBody = GetComponent<Rigidbody>();
         initialPos = transform.position;
+        targetInitialPos = target.position;
     }
 
     public override void AgentReset()
@@ -22,9 +28,9 @@ public class RollerAgent : Agent
         this.rBody.velocity = Vector3.zero;
         this.transform.position = initialPos;
 
-        target.position = new Vector3(initialPos.x + Random.Range(-7, 7),
-                              initialPos.y,
-                              initialPos.z + Random.Range(-7, 7));
+        target.position = new Vector3(targetInitialPos.x + Random.Range(-7, 7),
+                                      targetInitialPos.y,
+                                      targetInitialPos.z + Random.Range(-3, 3));
     }
 
     public override void CollectObservations()
@@ -32,10 +38,10 @@ public class RollerAgent : Agent
         // Target and Agent positions
         AddVectorObs(target.position);
         AddVectorObs(this.transform.position);
+        AddVectorObs(wall.position);
 
         // Agent velocity
-        AddVectorObs(rBody.velocity.x);
-        AddVectorObs(rBody.velocity.z);
+        AddVectorObs(rBody.velocity);
     }
 
     public override void AgentAction(float[] vectorAction)
@@ -45,10 +51,19 @@ public class RollerAgent : Agent
         // to the AgentAction() function. The number of elements in this
         // array is determined by the Vector Action Space Type and Space Size
         // settings of the agent's Brain. 
-        Vector3 controlSignal = Vector3.zero;
-        controlSignal.x = vectorAction[0];
-        controlSignal.z = vectorAction[1];
-        rBody.AddForce(controlSignal * speed);
+        var x = vectorAction[0];
+        var y = vectorAction[1];
+        var z = vectorAction[2];
+
+        if (y != 0 && isGrounded)
+        {
+            rBody.AddForce(new Vector3(x * speed, y * jumpForce, z * speed));
+            isGrounded = false;
+        }
+        else
+        {
+            rBody.AddForce(new Vector3(x * speed, 0, z * speed));
+        }
 
         // Rewards
         // Reinforcement learning requires rewards. Assign rewards in the
@@ -67,15 +82,6 @@ public class RollerAgent : Agent
 
             Done();
         }
-
-        if (this.transform.position.y < initialPos.y - 5.0f)
-        {
-            float reward = (1.0f - ((float)GetStepCount() / (float)maxStep)) * -100;
-            SetReward(reward);
-            debugText.text = "Previous reward: " + GetCumulativeReward();
-
-            Done();
-        }
     }
 
     // In order for the Agent to use the Heuristic, you will need to set the
@@ -86,9 +92,17 @@ public class RollerAgent : Agent
         // What this code means is that the heuristic will generate an action 
         // corresponding to the values of the "Horizontal" and "Vertical" input
         // axis (which correspond to the keyboard arrow keys).
-        var action = new float[2];
+        var action = new float[3];
+
         action[0] = Input.GetAxis("Horizontal");
-        action[1] = Input.GetAxis("Vertical");
+        action[1] = Input.GetKey(KeyCode.Space) ? 1.0f : 0.0f;
+        action[2] = Input.GetAxis("Vertical");
         return action;
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.collider.CompareTag("ground"))
+            isGrounded = true;
     }
 }
